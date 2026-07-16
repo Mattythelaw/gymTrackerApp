8f2a7c3cb950c66f66041c7227f86c12c0677aad
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-07-16 20:38:47 +0200

    Added secondary constructor for unknown MG and sets

--- app/src/main/java/com/example/gymtrackerapp/Exercise.kt
+++ app/src/main/java/com/example/gymtrackerapp/Exercise.kt
@@ -15,9 +15,13 @@ class Exercise(exerciseName: String, muscleGroup: String, sets: Int, reps: Strin
 
     var reps: String = " "
 
-    //Secondary constructor 1- user does not know muscleGroup but knows sets//
+    //Secondary constructor 1 - user does not know muscleGroup but knows sets//
     constructor( exerciseName: String, sets: Int, reps: String)
             : this(exerciseName, "Unknown Muscle Group", sets, reps )
+
+    //Secondary constructor 2 - user does not know muscle group and sets//
+    constructor(exerciseName: String, reps: String, placeholder: Boolean)
+            : this(exerciseName, "Unknown Muscle Group", 0, reps )
 
 
 }
