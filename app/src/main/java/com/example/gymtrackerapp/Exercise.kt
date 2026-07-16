405b5e54b56f7cbddede82f587542ddcd8f24157
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-07-16 20:31:23 +0200

    Added secondary constructor 1 - unknown muscleGroup

--- app/src/main/java/com/example/gymtrackerapp/Exercise.kt
+++ app/src/main/java/com/example/gymtrackerapp/Exercise.kt
@@ -6,7 +6,20 @@ import androidx.core.view.WindowInsetsCompat
 
 class Exercise(exerciseName: String, muscleGroup: String, sets: Int, reps: String ){
 
-    
+    //Adding variables for gym tracking attributes//
+    var name:  String = " "
+
+    var muscleGroup: String = " "
+
+    var sets: Int = 0
+
+    var reps: String = " "
+
+    //Secondary constructor 1- user does not know muscleGroup but knows sets//
+    constructor( exerciseName: String, sets: Int, reps: String)
+            : this(exerciseName, "Unknown Muscle Group", sets, reps )
+
+
 }
 
 
