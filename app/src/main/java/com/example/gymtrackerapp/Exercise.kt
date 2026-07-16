1a095ad6feb9b3439cefa6f31acdb778c5041622
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-07-16 21:02:34 +0200

    Added missing return true for boolean

--- app/src/main/java/com/example/gymtrackerapp/Exercise.kt
+++ app/src/main/java/com/example/gymtrackerapp/Exercise.kt
@@ -3,10 +3,10 @@ package com.example.gymtrackerapp
 import android.util.Log
 
 
-class Exercise(exerciseName: String, muscleGroup: String, sets: Int, reps: String ){
+class Exercise(exerciseName: String, muscleGroup: String, sets: Int, reps: String ) {
 
     //Adding variables for gym tracking attributes//
-    var name:  String = " "
+    var name: String = " "
 
     var muscleGroup: String = " "
 
@@ -15,15 +15,15 @@ class Exercise(exerciseName: String, muscleGroup: String, sets: Int, reps: Strin
     var reps: String = " "
 
     //Secondary constructor 1 - user does not know muscleGroup but knows sets//
-    constructor( exerciseName: String, sets: Int, reps: String)
-            : this(exerciseName, "Unknown Muscle Group", sets, reps )
+    constructor(exerciseName: String, sets: Int, reps: String)
+            : this(exerciseName, "Unknown Muscle Group", sets, reps)
 
     //Secondary constructor 2 - user does not know muscle group and sets//
     constructor(exerciseName: String, reps: String, placeholder: Boolean)
-            : this(exerciseName, "Unknown Muscle Group", 0, reps )
+            : this(exerciseName, "Unknown Muscle Group", 0, reps)
 
     //Initializing the varibales/attributes for the gym tracking//
-    init{
+    init {
         name = exerciseName
         this.muscleGroup = muscleGroup
         this.sets = sets
@@ -35,22 +35,28 @@ class Exercise(exerciseName: String, muscleGroup: String, sets: Int, reps: Strin
         Log.v("Exercise", "$name has started")
 
     }
+
     fun Complete() {
         Log.v("Exercise", "$name has been completed")
 
     }
+
     fun Skip() {
         Log.v("Exercise", "$name has been skipped")
 
     }
-    fun MarkPersonalBest() : Boolean {
-        Log.v("Exercise", "$name is your personal best")
 
+    fun MarkPersonalBest(): Boolean {
+        Log.v("Exercise", "$name is your personal best")
+        //returning placeholder for boolean
+        return true
     }
-    fun DisplayExercise( ) : String {
+
+    fun DisplayExercise(): String {
         return "$name - $muscleGroup (Sets: $sets, Reps: $reps)"
 
     }
+
 
 }
 
