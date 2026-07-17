835dd5d8a03fd3c8c934f10fe1eeac0783e43822
Matthew Lawrence <matthewconnor0418@gmail.com>
2026-07-17 11:38:41 +0200

    Added textviews to mainActitvity + linked id

--- app/src/main/java/com/example/gymtrackerapp/MainActivity.kt
+++ app/src/main/java/com/example/gymtrackerapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.gymtrackerapp
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +12,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+    val displayExercise = findViewById<TextView>(R.id.displayExercise)
+    var exerciseTxt = " "
+
+        
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
